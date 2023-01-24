2b7090e3ffa00f469285bcc1e949682526872c8f
khizarali646 <khizarali646@gmail.com>
2023-01-24 16:47:33 +0500

    add origin cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
   });
   await app.listen(process.env.PORT || 3000);
 }
